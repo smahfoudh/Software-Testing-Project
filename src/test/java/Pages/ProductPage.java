85bc9a3a6137066a9b1fc4ec1e4952196adf7f15
smahfoudh <skan.mh@gmail.com>
2022-12-07 22:38:42 +0100

    finished cart.feature and its step definitions

--- src/test/java/Pages/ProductPage.java
+++ src/test/java/Pages/ProductPage.java
@@ -3,6 +3,7 @@ package Pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class ProductPage {
@@ -11,7 +12,7 @@ public class ProductPage {
 
     public static final String URL= "https://www.saucedemo.com/inventory.html"; //our page url
 
-    private WebDriver driver; //our webdriver
+    private WebDriver driver; //our web driver
 
     /**
      * our ProductPage constructor
@@ -21,7 +22,7 @@ public class ProductPage {
         this.driver = driver;
     }
 
-    //loading our page webelements
+    //loading our page web elements
     public void loadProductPage(){PageFactory.initElements(driver, this);}
 
     /**
@@ -42,7 +43,124 @@ public class ProductPage {
         getField(locator).sendKeys(value);
     }
 
+    //-----------------------------------
 
 
+    //The web element for the cart button.
+    @FindBy(className = "shopping_cart_link")
+    public WebElement shoppingCart;
+
+    //The web element for the checkout button.
+    @FindBy(xpath = "//*[@id='checkout']")
+    private WebElement checkoutButton;
+
+    //side menu button web element.
+    @FindBy(xpath = "//*[@id='react-burger-menu-btn']")
+    private WebElement menuButton;
+
+    //reset app state button web element.
+    @FindBy(xpath = "//*[@id='reset_sidebar_link']")
+    private WebElement resetAppStateButton;
+
+    //The web element for firstname when filling checkout form
+    public static final By FIRSTNAME = By.id("first-name");
+
+    //The web element for lastname when filling checkout form
+    public static final By LASTNAME = By.id("last-name");
+
+    //The web element for zipcode when filling checkout form
+    public static final By ZIPCODE = By.id("postal-code");
+
+     //The web element for  adding sauce labs backpack to cart button.
+    @FindBy(xpath = "//*[@id='add-to-cart-sauce-labs-backpack']")
+    private WebElement addBackpackButton;
+
+    //The web element for adding Sauce Labs Bike Light to cart button.
+    @FindBy(xpath = "//*[@id='add-to-cart-sauce-labs-bike-light']")
+    private WebElement addBikeLightButton;
+
+    //The web element for adding Sauce Labs Bolt T-Shirt to cart button.
+    @FindBy(xpath = "//*[@id='add-to-cart-sauce-labs-bolt-t-shirt']")
+    private WebElement addBoltTshirtButton;
+
+    //The web element for adding Sauce Labs Fleece Jacket to cart button.
+    @FindBy(xpath = "//*[@id='add-to-cart-sauce-labs-fleece-jacket']")
+    private WebElement addFleeceJacketButton;
+
+    //The web element for adding Sauce Labs Onesie to cart button.
+    @FindBy(xpath = "//*[@id='add-to-cart-sauce-labs-onesie']")
+    private WebElement addOnesieButton;
+
+    //The web element for adding Test.allTheThings() T-Shirt (Red) to cart button.
+    @FindBy(xpath = "//*[@id='add-to-cart-test.allthethings()-t-shirt-(red)']")
+    private WebElement addRedTshirtButton;
+
+    //The web element for continue button.
+    @FindBy(xpath = "//*[@id='continue']")
+    private WebElement continueButton;
+
+
+    //total value web element
+    @FindBy(xpath = "//div[@class=\"summary_total_label\"]")
+    private WebElement totalLabel;
+
+    //clicking on items webelements:
+
+    //opening shopping cart
+    public void clickOnCart(){
+        shoppingCart.click();
+    }
+
+    //adding Backpack to cart button.
+    public void clickAddBackpackButton(){
+        addBackpackButton.click();
+    }
+
+    //adding Bike Light to cart button.
+    public void clickAddBikeLightButton(){
+        addBikeLightButton.click();
+    }
+
+    //adding Bolt Tshirt to cart button.
+    public void clickAddBoltTshirtButton(){
+        addBoltTshirtButton.click();
+    }
+
+    //adding Fleece Jacket to cart button.
+    public void clickAddFleeceButton(){
+        addFleeceJacketButton.click();
+    }
+
+    //adding the Onesie to cart button.
+    public void clickAddOnesieButton(){
+        addOnesieButton.click();
+    }
+
+    //adding Test.allTheThings() T-Shirt (Red) to cart button.
+    public void clickAddAllThingsShirtButton(){
+        addRedTshirtButton.click();
+    }
+
+    //clicking checkout button
+    public void clickCheckoutButton(){
+        checkoutButton.click();
+    }
+
+    //clicking continue button
+    public void clickContinueButton(){
+        continueButton.click();
+    }
+
+    //getting the text from the total web element
+    public String getTotal(){return totalLabel.getText();}
+
+
+    //clicking on the menu button web element.
+    public void clickMenuButton(){menuButton.click();}
+
+    //clicking on reset app state web element.
+    public void clickReset(){
+        resetAppStateButton.click();
+    }
 
 }
--- src/test/java/StepDefinitions/CartStepDefinitions.java
+++ src/test/java/StepDefinitions/CartStepDefinitions.java
@@ -1,23 +1,85 @@
 package StepDefinitions;
 
-import  Pages.InventoryPage;
-import io.cucumber.java.AfterAll;
+import  Pages.ProductPage;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import org.junit.Assert;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 
 
         //defining our steps for cart.feature
 public class CartStepDefinitions extends AbsStepDefinitions {
 
+        @Given("{string} is added to my cart")
+        public void ItemAddedToMyCart(String itemName) {
+                String[] items = itemName.split(",");
+                for (String item : items){
+                        if(item.equals("Sauce Labs Backpack")){
+                                productPage.clickAddBackpackButton();
+                                //break;
+                        }
+                        if(item.equals("Sauce Labs Bike Light")){
+                                productPage.clickAddBikeLightButton();
+                               // break;
+                        }
+                        if(item.equals("Sauce Labs Bolt T-Shirt")){
+                                productPage.clickAddBoltTshirtButton();
+                             //   break;
+                        }
+                        if(item.equals("Sauce Labs Fleece Jacket")){
+                                productPage.clickAddFleeceButton();
+                             //   break;
+                        }
+                        if(item.equals("Sauce Labs Onesie")){
+                                productPage.clickAddOnesieButton();
+                             //   break;
+                        }
+                        if(item.equals("Test.allTheThings() T-Shirt (Red)")){
+                                productPage.clickAddAllThingsShirtButton();
+                              //  break;
+                        }
+                }
+        }
+
+        @Given("cart is displayed")
+        public void cartDisplayed(){productPage.clickOnCart();}
+
+        @And("checkout button is clicked")
+        public void checkoutButtonClicked(){productPage.clickCheckoutButton();}
+
+        @And("the first name is filled with {string}")
+        public void fillFirstName(String firstName){
+                productPage.fillField(ProductPage.FIRSTNAME,firstName);
+        }
+
+        @And("the last name is filled with {string}")
+        public void fillLastName(String lastName){
+                productPage.fillField(ProductPage.LASTNAME,lastName);
+        }
+
+        @And("the zip code is filled with {string}")
+        public void fillZipCode(String zipCode){
+                 productPage.fillField(ProductPage.ZIPCODE,zipCode);
+                }
+
+        @When("continue button is clicked")
+        public void continueButtonClicked() {
+                        productPage.clickContinueButton();
+                }
+
+        @Then("{string} is shown")
+        public void showTotal(String total) {
+           Assertions.assertEquals(total, productPage.getTotal()); //after getting the total we use the menu to reset
+           productPage.clickMenuButton();
+           productPage.clickReset();
+                }
+
+        @Given("Sauce Labs Backpack is added to my cart")
+        public void addBackPack(){productPage.clickAddBackpackButton();}
+
+        }
 
 
 
 
-
-
-}

c68f45646e22a94c9f32a75cec2b3ea6c18aa2ff
smahfoudh <skan.mh@gmail.com>
2022-12-07 18:24:14 +0100

    added readme file

---
+++ src/test/java/StepDefinitions/CartStepDefinitions.java
@@ -0,0 +1,23 @@
+package StepDefinitions;
+
+import  Pages.InventoryPage;
+import io.cucumber.java.AfterAll;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+
+
+        //defining our steps for cart.feature
+public class CartStepDefinitions extends AbsStepDefinitions {
+
+
+
+
+
+
+
+}
---
+++ src/test/resources/Features/cart.feature
@@ -0,0 +1,7 @@
+Feature: Saucedemo cart testing.
+
+  Background:
+    Given homepage is open
+    And Username is filled with 'standard_user'
+    And Password is filled with 'secret_sauce'
+    And Login button is click ed
